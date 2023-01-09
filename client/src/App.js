dbccbd2e812a2efe4c89d9c08678b724e8df889b
Maulana Akbar Yudistika <yudistika.akbar@gmail.com>
2023-01-09 14:04:56 +0700

    add client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/u/:userId" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ const router = require("./routes/user-routes");
 const uri = process.env.MONGODB_URI;
 const port = 3001;
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 
-// app.use(cors());
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
@@ -16,10 +16,11 @@ app.use("/api", router);
 (async () => {
   try {
     await mongoose.connect(uri);
-    app.listen(port, () => {
-      console.log(`Database is connected! Server running on ${port}`);
-    });
   } catch (err) {
     console.log(`There is an error ${err.message}`);
   }
+
+  app.listen(port, () => {
+    console.log(`Database is connected! Server running on ${port}`);
+  });
 })();
